380344e7b0cfa805f95604c770b4bdc55a35f434
Evgeny Kurnevsky <kurnevsky@gmail.com>
2018-03-03 14:56:28 +0300

    Make tt-rss working.

--- roles/tt-rss/files/config.php
+++ roles/tt-rss/files/config.php
@@ -18,7 +18,7 @@
         // *** Basic settings (important!) ***
         // ***********************************
 
-        define('SELF_URL_PATH', 'http://example.org/tt-rss/');
+        define('SELF_URL_PATH', 'https://192.168.99.48/tt-rss/');
         // Full URL of your tt-rss installation. This should be set to the
         // location of tt-rss directory, e.g. http://example.org/tt-rss/
         // You need to set this option correctly otherwise several features
